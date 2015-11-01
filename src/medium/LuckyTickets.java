ea48a2e3fc93b3fd50719037061f206fbb8e5629
sergey-zhidkov <sergey.zhidkov@outlook.com>
2015-10-31 23:32:29 -0700

    Fix for Lucky Tickets. Even 'long' is not enough. Use 'BigInteger'.

--- src/medium/LuckyTickets.java
+++ src/medium/LuckyTickets.java
@@ -4,15 +4,28 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LuckyTickets {
 
-    private static Map<Integer, int []> map = new HashMap<>();
+    private static Map<Integer, BigInteger []> map = new HashMap<>();
+    private static final BigInteger ZERO = BigInteger.valueOf(0);
 
     public static void main (String[] args) throws IOException {
-        map.put(1, new int [] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}); // for level 1
+        map.put(1, new BigInteger[] {
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1"),
+                new BigInteger("1")
+        }); // for level 1
         File file = new File(args[0]);
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String line;
@@ -22,11 +35,11 @@ public class LuckyTickets {
         }
     }
 
-    static long countOfLuckyTickets(String line) {
+    static BigInteger countOfLuckyTickets(String line) {
         int digitsCount = Integer.parseInt(line) / 2;
 
         if (digitsCount == 1) {
-            return 10;
+            return BigInteger.valueOf(10);
         }
 
         buildTableForLevel(digitsCount);
@@ -34,10 +47,10 @@ public class LuckyTickets {
         return sumAllSums(map.get(digitsCount));
     }
 
-    static long sumAllSums(int [] sumCount) {
-        long result = 0;
-        for (int sum : sumCount) {
-            result += (long) sum * (long) sum;
+    static BigInteger sumAllSums(BigInteger [] sumCount) {
+        BigInteger result = ZERO;
+        for (BigInteger sum : sumCount) {
+            result = sum.multiply(sum).add(result);
         }
         return result;
     }
@@ -49,20 +62,20 @@ public class LuckyTickets {
         }
 
         for (int i = curLevel + 1; i <= level; i++) {
-            int [] prevLevel = map.get(i - 1);
+            BigInteger [] prevLevel = map.get(i - 1);
             map.put(i, buildLevel(prevLevel, i));
         }
     }
 
-    static int [] buildLevel(int [] prevLevel, int level) {
-        int [] newLevel = new int[9 * level + 1];
+    static BigInteger [] buildLevel(BigInteger [] prevLevel, int level) {
+        BigInteger [] newLevel = new BigInteger[9 * level + 1];
         for (int i = 0; i < newLevel.length; i++) {
             newLevel[i] = getSumTenPrevSum(prevLevel, i);
         }
         return newLevel;
     }
 
-    static int getSumTenPrevSum(int [] level, int index) {
+    static BigInteger getSumTenPrevSum(BigInteger [] level, int index) {
         int startIndex = index - 9;
         if (startIndex < 0) {
             startIndex = 0;
@@ -71,9 +84,9 @@ public class LuckyTickets {
         if (index >= level.length) {
             index = level.length - 1;
         }
-        int result = 0;
+        BigInteger result = ZERO;
         for (int i = startIndex; i <= index; i++) {
-            result += level[i];
+            result = result.add(level[i]);
         }
         return result;
     }

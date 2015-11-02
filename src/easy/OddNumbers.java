b61fd2f8b52798945fe61565cd171738b8a26cd6
sergey-zhidkov <sergey.zhidkov@outlook.com>
2015-11-01 19:10:34 -0800

    Solve Odd Numbers challenge

---
+++ src/easy/OddNumbers.java
@@ -0,0 +1,11 @@
+package easy;
+
+import java.io.IOException;
+
+public class OddNumbers {
+    public static void main (String[] args) throws IOException {
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
